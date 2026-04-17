37c4547f1c0c021f68418ba2006130cb9ddeff2b
Megaman <pacosclub@gmail.com>
2026-04-16 18:44:30 -0600

    feat(Routes): Creación de routes

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const { createUser, login } = require('./controllers/users');
+const auth = require('./middleware/auth');
+const usersRoutes = require('./routes/users');
+const articlesRoutes = require('./routes/articles');
+const NotFoundError = require('./errors/not-found-err');
+const { validateUser, validateLogin } = require('./middleware/validation');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.post('/signin', validateLogin, login);
+app.post('/signup', validateUser, createUser);
+app.use('/users', auth, usersRoutes);
+app.use('/articles', auth, articlesRoutes);
+app.use('/', () => {
+  throw new NotFoundError('Recurso solicitado no encontrado');
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor encendido en el puerto ${PORT}`);
